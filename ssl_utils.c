5d1317aac8c77d6d4debe1c02e1898d5b328ba48
Amin Ben Ramdhane <amin.benramdhane@pivasoftware.com>
2022-04-05 17:50:43 +0100

    Add support for libmbedtls

--- ssl_utils.c
+++ ssl_utils.c
@@ -18,13 +18,63 @@
  * 02110-1301 USA
  */
 
+#ifdef LMBEDTLS
+#include <mbedtls/md.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
+#else
 #include <openssl/ssl.h>
 #include <openssl/hmac.h>
 #include <openssl/rand.h>
+#endif
+
 #include <string.h>
 
 #include "common.h"
 #include "log.h"
+
+static int rand_bytes(unsigned char *output, size_t len)
+{
+#ifdef LMBEDTLS
+	mbedtls_entropy_context ec = {0};
+	mbedtls_ctr_drbg_context cd_ctx = {0};
+	int res = 1;
+
+	union {
+		uint64_t seed;
+		uint8_t buffer[8];
+	} rand_buffer;
+
+	FILE *urand = fopen("/dev/urandom", "r");
+	if (urand) {
+		fread(&rand_buffer.seed, sizeof(rand_buffer.seed), 1, urand);
+		fclose(urand);
+	} else {
+		rand_buffer.seed = (uint64_t)clock();
+	}
+
+	mbedtls_entropy_init(&ec);
+	mbedtls_ctr_drbg_init(&cd_ctx);
+
+	if (mbedtls_ctr_drbg_seed(&cd_ctx, mbedtls_entropy_func, &ec, (const unsigned char *)rand_buffer.buffer, 8) != 0) {
+		CWMP_LOG(ERROR, "Failed to initialize random generator\n");
+		res = -1;
+		goto end;
+	}
+
+	if (mbedtls_ctr_drbg_random(&cd_ctx, output, len) != 0) {
+		CWMP_LOG(ERROR, "Failed to generate random bytes\n");
+		res = -1;
+	}
+
+end:
+	mbedtls_ctr_drbg_free(&cd_ctx);
+	mbedtls_entropy_free(&ec);
+	return res;
+#else
+	return RAND_bytes(output, len);
+#endif
+}
 
 char *generate_random_string(size_t size)
 {
@@ -37,7 +87,7 @@ char *generate_random_string(size_t size)
 		goto end;
 	}
 
-	int written = RAND_bytes(buf, size);
+	int written = rand_bytes(buf, size);
 	if (written != 1) {
 		CWMP_LOG(ERROR,"Failed to get random bytes");
 		goto end;
@@ -45,7 +95,7 @@ char *generate_random_string(size_t size)
 
 	hex = string_to_hex(buf, size);
 	if (hex == NULL)
-			goto end;
+		goto end;
 
 	hex[size] = '\0';
 
@@ -56,22 +106,26 @@ end:
 
 void message_compute_signature(char *msg_out, char *signature, size_t len)
 {
-	int i;
 	int result_len = 20;
-	unsigned char *result;
 	struct cwmp *cwmp = &cwmp_main;
 	struct config *conf;
 	conf = &(cwmp->conf);
-	/*	unsigned char *HMAC(const EVP_MD *evp_md, const void *key, int key_len,
-	                    const unsigned char *d, size_t n, unsigned char *md,
-	                    unsigned int *md_len);*/
-	result = HMAC(EVP_sha1(), conf->acs_passwd, strlen(conf->acs_passwd), (unsigned char *)msg_out, strlen(msg_out), NULL, NULL);
-	for (i = 0; i < result_len; i++) {
+
+#ifdef LMBEDTLS
+	unsigned char result[MBEDTLS_MD_MAX_SIZE] = {0};
+	const mbedtls_md_info_t *md_info = mbedtls_md_info_from_type(MBEDTLS_MD_SHA1);
+
+	mbedtls_md_hmac(md_info, (unsigned char *)conf->acs_passwd, strlen(conf->acs_passwd), (unsigned char *)msg_out, strlen(msg_out), result);
+#else
+	unsigned char result[EVP_MAX_MD_SIZE] = {0};
+
+	HMAC(EVP_sha1(), conf->acs_passwd, strlen(conf->acs_passwd), (unsigned char *)msg_out, strlen(msg_out), result, NULL);
+#endif
+
+	for (int i = 0; i < result_len; i++) {
 		if (len - strlen(signature) < 3) // each time 2 hex chars + '\0' at end so needed space is 3 bytes
 			break;
 
 		snprintf(&(signature[i * 2]), 3, "%02X", result[i]);
 	}
-	FREE(result);
 }
-
